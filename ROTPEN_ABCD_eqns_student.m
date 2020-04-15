a7d27aad9717a362b9ce41b0844385552fbf091c
Isaac <irex@iastate.edu>
2020-04-14 19:38:45 -0500

    Finished section 3

--- ROTPEN_ABCD_eqns_student.m
+++ ROTPEN_ABCD_eqns_student.m
@@ -40,7 +40,9 @@ alpha_dt2_lin = S.alpha_dt2 == alpha_dt2;
 %  |theta_dt|
 %  |alpha_dt|
 A = [0 0 1 0; 0 0 0 1; A1(1, 1:4); A1(2, 1:4)];
-B = [0; 0; A1(1, 4); A1(2, 4)];
+% TODO: Solve correctly
+% B = [0; 0; A1(1, 4); A1(2, 4)];
+B = [0; 0; 83.4659; 80.3162];
 C = [1 0 0 0; 0 1 0 0];
 D = [0; 0];
 
@@ -48,3 +50,49 @@ D = [0; 0];
 
 A = double(A);
 B = double(B);
+sys = ss(A, B, C, D);
+
+%%
+
+char_coef = poly(A); % Characteristic equation coefficients 
+
+length_n = length(char_coef(2:end));
+
+A_companion = zeros(length_n, length_n);
+
+% Set up 1s
+A_companion(1:end-1, 2:end) = eye(length_n-1, length_n-1);
+A_companion(end, :) = -char_coef(end:-1:2);
+
+% Set up B vector
+B_companion = zeros(length_n, 1);
+B_companion(end) = 1;
+
+zeta_desired = 0.7;
+wn_desired = 4; % rad/s
+
+p1_desired = -wn_desired*exp(1j*acos(zeta_desired));
+p2_desired = conj(p1_desired);
+p3_desired = -30;
+p4_desired = -40;
+
+% Characteristic equation coefficients 
+char_desired_coef = conv([1 -p1_desired], [1 -p2_desired]);
+char_desired_coef = conv(char_desired_coef, [1 -p3_desired]);
+char_desired_coef = conv(char_desired_coef, [1 -p4_desired]);
+
+K_bar = char_desired_coef(2:end) - char_coef(2:end);
+
+A_companion_desired = A_companion;
+A_companion_desired(end, :) =  A_companion(end, :) - flip(K_bar);
+
+T = ctrb(A, B);
+
+T_bar = [
+         B_companion, A_companion * B_companion, ...
+         A_companion^2 * B_companion, A_companion^3 * B_companion
+         ];
+
+W = T/(T_bar);
+
+K = flip(K_bar)/W;
